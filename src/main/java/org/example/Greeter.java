692e0be8b5e2baee90d2c0c4acadabf1bb2a9ba2
janeforjane <janeforjane@mail.ru>
2022-10-13 15:37:37 +0300

    fix

--- src/main/java/org/example/Greeter.java
+++ src/main/java/org/example/Greeter.java
@@ -5,4 +5,8 @@ public class Greeter {
     public String hello(String name) {
         return "Hello " + name;
     }
+
+    public String bye(String name) {
+        return "Bye " + name;
+    }
 }
